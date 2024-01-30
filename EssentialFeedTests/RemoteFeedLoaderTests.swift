1a7accf521d12b0acf206136f0cbb8d9de96cb7d
Joffrey Mann <joffrey@jfammobility.com>
2024-01-30 07:54:45 -0500

    `RemoteFeedLoader` does not request data upon creation
    `RemoteFeedLoader` does not request data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,24 @@
+//
+//  RemoteFeedLoaderTests.swift
+//
+//  Created by Joffrey Mann on 1/30/24.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+                
+        XCTAssertNil(client.requestedURL)
+    }
+}
