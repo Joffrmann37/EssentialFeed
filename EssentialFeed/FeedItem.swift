cb72ed236d8019c31b95c7a95817eab01bc91938
Joffrey Mann <joffrey@jfammobility.com>
2024-01-29 22:13:13 -0500

    Initial Model added

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Joffrey Mann on 1/29/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
