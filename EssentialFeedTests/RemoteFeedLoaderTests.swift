98e1e325830ecbf70fcb0a5100be2ed597875552
Joffrey Mann <joffrey@jfammobility.com>
2024-02-01 11:20:09 -0500

    Delivers invalidData error on 200 HTTP response with invalid json
    Delivers invalidData error on 200 HTTP response with invalid json

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -57,6 +57,17 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        
+        sut.load { capturedErrors.append($0) }
+        let invalidJSON = Data("invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, spy: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
@@ -69,23 +80,23 @@ class RemoteFeedLoaderTests: XCTestCase {
             return results.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             let feedLoaderResult = FeedLoaderResult(url: url, completion: completion)
             results.append(feedLoaderResult)
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            results[index].completion(error, nil)
+            results[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode statusCode: Int, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)
-            results[index].completion(nil, response)
+        func complete(withStatusCode statusCode: Int, data: Data = Data(), at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+            results[index].completion(.success(data, response))
         }
     }
 }
 
 public struct FeedLoaderResult {
     var url: URL
-    var completion: (Error?, HTTPURLResponse?) -> Void
+    var completion: (HTTPClientResult) -> Void
 }
