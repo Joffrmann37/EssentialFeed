30963b5b2c1af1321490e12cc38d6c1efe825e96
Joffrey Mann <joffrey@jfammobility.com>
2024-02-05 17:23:40 -0500

    Delivers items array on 200 HTTP response with items
    Delivers items array on 200 HTTP response with items

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -43,8 +43,8 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, _):
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(.init(errorType: .invalidData)))
                 }
@@ -53,4 +53,8 @@ public final class RemoteFeedLoader {
             }
         }
     }
+}
+
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -7,9 +7,19 @@
 
 import Foundation
 
-public struct FeedItem: Codable, Equatable {
+public struct FeedItem: Equatable {
     let id: UUID
     let description: String?
     let location: String?
     let imageURL: URL
+}
+
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
