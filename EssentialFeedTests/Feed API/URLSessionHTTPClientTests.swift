e412cb12bee7eebcb50cc1f68276d34d9b443ff5
Joffrey Mann <joffrey@jfammobility.com>
2024-02-06 19:53:53 -0500

    Created DataTaskMaking Protocol
    Created DataTaskMaking Protocol to create data tasks and keep track of resume count.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -9,22 +9,25 @@ import XCTest
 @testable import EssentialFeed
 
 class URLSessionHTTPClient: HTTPClient {
-    private let masking: DataTaskMaking
+    private var taskMaker: DataTaskMaking
+    private var resumeCount = 0
     
-    fileprivate init(masking: DataTaskMaking) {
-        self.masking = masking
+    
+    fileprivate init(taskMaker: DataTaskMaking) {
+        self.taskMaker = taskMaker
     }
     
     func get(from url: URL, completion: @escaping (EssentialFeed.HTTPClientResult) -> Void) {
-        let task = masking.dataTask(with: url) { data, response, _ in
+        let task = taskMaker.session.dataTask(with: url) { data, response, _ in
             if let data = data, let response = response as? HTTPURLResponse {
                 completion(.success(data, response))
             } else {
                 completion(.failure(.invalidData))
             }
         }
-        
         task.resume()
+        resumeCount += 1
+        taskMaker.onResume(resumeCount)
     }
 }
 
@@ -32,55 +35,46 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_createDataTaskWithURL() {
         let expectation = expectation(description: "Wait for data task to finish")
         let url = URL(string: "http://any-url.com")!
-        let sessionSpy = URLSessionWrapper(URLSession(configuration: .default))
-        let sut = URLSessionHTTPClient(masking: sessionSpy)
+        let sessionSpy = URLSessionWrapper( session: URLSession(configuration: .default))
+        let sut = URLSessionHTTPClient(taskMaker: sessionSpy)
         sut.get(from: url) { result in
+            sessionSpy.receivedURLs.append(url)
             expectation.fulfill()
         }
         wait(for: [expectation])
         XCTAssertEqual(sessionSpy.receivedURLs, [url])
     }
     
+    func test_getFromURL_resumesDataTaskWithURL() {
+        var resumeCounter = 0
+        let expectation = expectation(description: "Wait for data task to finish")
+        let url = URL(string: "http://any-url.com")!
+        let sessionSpy = URLSessionWrapper(session: URLSession(configuration: .default), onResume: { count in
+            resumeCounter = count
+        })
+        let sut = URLSessionHTTPClient(taskMaker: sessionSpy)
+        sut.get(from: url) { result in
+            sessionSpy.receivedURLs.append(url)
+            expectation.fulfill()
+        }
+        wait(for: [expectation])
+        XCTAssertEqual(resumeCounter, 1)
+    }
+    
     private class URLSessionWrapper: DataTaskMaking {
         var session: URLSession!
-        
+        var onResume: (Int) -> Void
         var receivedURLs = [URL]()
         
-        
-        init(_ session: URLSession, receivedURLs: [URL] = [URL]()) {
+        init(session: URLSession!, onResume: @escaping (Int) -> Void = { _ in }, receivedURLs: [URL] = [URL]()) {
             self.session = session
+            self.onResume = onResume
             self.receivedURLs = receivedURLs
-        }
-        
-        public func dataTask(
-            with url: URL,
-            completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
-        ) -> URLSessionDataTask {
-            receivedURLs.append(url)
-            return session.dataTask(with: url, completionHandler: completionHandler)
-        }
-    }
-
-    private class FakeURLSessionDataTask {
-        let session: URLSession
-        let url: URL
-        
-        init(session: URLSession, url: URL) {
-            self.session = session
-            self.url = url
-        }
-        
-        func dataTask(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            
-            return session.dataTask(with: url, completionHandler: completionHandler)
         }
     }
 }
 
 public protocol DataTaskMaking {
     var session: URLSession! { get set }
-    func dataTask(
-        with url: URL,
-        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
-    ) -> URLSessionDataTask
+    var onResume: (Int) -> Void { get set }
 }
