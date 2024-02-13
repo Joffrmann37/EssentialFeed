6833b41177cd86e1b3dc8a8318f164bc843a62c6
Joffrey Mann <joffrey@jfammobility.com>
2024-02-13 11:57:56 -0500

    Modified protocol
    Modified protocol to have task and result

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -15,22 +15,20 @@ public struct DataTaskResult {
 }
 
 class URLSessionHTTPClient: HTTPClient {
-    private var task: URLSessionDataTask
+    private var session: HTTPSession
     private var onResume: (Int) -> Void
-    private var result: DataTaskResult
     private var resumeCount = 0
     
     
-    fileprivate init(task: URLSessionDataTask, onResume: @escaping (Int) -> Void = { _ in }, result: DataTaskResult) {
-        self.task = task
+    fileprivate init(session: HTTPSession, onResume: @escaping (Int) -> Void = { _ in }) {
+        self.session = session
         self.onResume = onResume
-        self.result = result
     }
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        if let data = result.data, let response = result.response {
+        if let data = session.taskResult.data, let response = session.taskResult.response {
             completion(.success(data, response))
-        } else if let error = result.error {
+        } else if let error = session.taskResult.error {
             completion(.failure(error))
         }
         resumeCount += 1
@@ -42,13 +40,12 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_resumesDataTaskWithURL() {
         var resumeCounter = 0
         let url = URL(string: "http://any-url.com")!
-        let (task, expectation, sessionSpy) = testTaskWithExpectation(url: url, error: nil, fulfillmentCount: 2)
-        task.resume()
-        sessionSpy.stub(url: url, task: task)
+        let (expectation, sessionSpy) = testTaskWithExpectation(url: url, error: nil, fulfillmentCount: 2)
+        sessionSpy.getStubs()[url]!.task.resume()
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            let sut = URLSessionHTTPClient(task: task, onResume: { count in
+            let sut = URLSessionHTTPClient(session: sessionSpy, onResume: { count in
                 resumeCounter = count
-            }, result: sessionSpy.taskResult)
+            })
             sut.get(from: url) { result in
                 expectation.fulfill()
             }
@@ -60,11 +57,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: RemoteFeedLoader.Error.errorDomain, code: 0)
-        let (task, expectation, sessionSpy) = testTaskWithExpectation(url: url, error: error)
-        task.resume()
+        let (expectation, sessionSpy) = testTaskWithExpectation(url: url, error: error)
+        sessionSpy.getStubs()[url]!.task.resume()
         wait(for: [expectation])
-        sessionSpy.stub(url: url, task: task)
-        let sut = URLSessionHTTPClient(task: sessionSpy.getStubs()[url]!.task, result: sessionSpy.taskResult)
+        let sut = URLSessionHTTPClient(session: sessionSpy)
         sut.get(from: url) { result in
             switch result {
             case .failure(let receivedError):
@@ -76,25 +72,20 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: Helper Functions
-    private func testTaskWithExpectation(url: URL, error: NSError? = nil, fulfillmentCount: Int = 1) -> (URLSessionDataTask, XCTestExpectation, URLSessionWrapper) {
+    private func testTaskWithExpectation(url: URL, error: NSError? = nil, fulfillmentCount: Int = 1) -> (XCTestExpectation, URLSessionWrapper) {
         let expectation = expectation(description: "Wait for data task to finish")
         expectation.expectedFulfillmentCount = fulfillmentCount
         let session = URLSession(configuration: .default)
         let sessionSpy = URLSessionWrapper(session: session)
-        let task = session.dataTask(with: url) { data, response, error in
-            guard let response = response as? HTTPURLResponse else {
-                sessionSpy.taskResult = DataTaskResult(data: data, response: nil, error: RemoteFeedLoader.Error.connectivity)
-                expectation.fulfill()
-                return
-            }
-            sessionSpy.taskResult = DataTaskResult(data: data, response: response, error: RemoteFeedLoader.Error.connectivity)
+        let task = sessionSpy.dataTask(with: url) {
             expectation.fulfill()
         }
         sessionSpy.stub(url: url, task: task, error: error)
-        return (task, expectation, sessionSpy)
+        return (expectation, sessionSpy)
     }
     
-    private class URLSessionWrapper: DataTaskMaking {
+    private class URLSessionWrapper: HTTPSession {
+        var task: URLSessionDataTask!
         var session: URLSession!
         var taskResult: DataTaskResult!
         private var stubs = [URL: Stub]()
@@ -108,17 +99,21 @@ class URLSessionHTTPClientTests: XCTestCase {
             self.session = session
         }
         
-        func stub(url: URL, task: URLSessionDataTask, error: Error? = nil) {
+        func stub(url: URL, task: URLSessionDataTask, error: Error?) {
             stubs[url] = Stub(task: task, error: error)
         }
         
-        func dataTask(with url: URL, completionHandler: @escaping @Sendable (DataTaskResult) -> Void) -> URLSessionDataTask {
-            guard let stub = stubs[url] else {
-                fatalError("Couldn't find stub for \(url)")
+        func dataTask(with url: URL, completionHandler: @escaping @Sendable () -> Void) -> URLSessionDataTask {
+            let task = session.dataTask(with: url) { data, response, error in
+                guard let response = response as? HTTPURLResponse else {
+                    self.taskResult = DataTaskResult(data: data, response: nil, error: RemoteFeedLoader.Error.connectivity)
+                    completionHandler()
+                    return
+                }
+                self.taskResult = DataTaskResult(data: data, response: response, error: RemoteFeedLoader.Error.connectivity)
+                completionHandler()
             }
-            
-            completionHandler(taskResult)
-            return stub.task
+            return task
         }
         
         func getStubs() -> [URL: Stub] {
@@ -127,7 +122,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
 }
 
-public protocol DataTaskMaking {
-    var session: URLSession! { get set }
-    func dataTask(with url: URL, completionHandler: @escaping @Sendable (DataTaskResult) -> Void) -> URLSessionDataTask
+public protocol HTTPSession {
+    var task: URLSessionDataTask! { get set }
+    var taskResult: DataTaskResult! { get set }
 }
